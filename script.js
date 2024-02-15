6430b9c483db4c5636da1cf0b53bc4a18929584f
yazeedcrash <140242342+yazeedcrash@users.noreply.github.com>
2024-02-15 13:57:06 +0300

    v1

---
+++ script.js
@@ -0,0 +1,7 @@
+// let username = document.getElementById("e").value();
+// let password = document.getElementById("p").value();
+// let number= document.getElementById("t").value();
+// let select= document.getElementById("s").value();
+function onChange(value){
+    console.log(value);
+}
